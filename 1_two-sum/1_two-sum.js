771cf745a365d5ec2bf6c04423e2841e98eee98d
sigristarisa <arisasigrist.ch@gmail.com>
2022-11-07 15:43:50 +0100

    finished day 1

---
+++ 1_two-sum/1_two-sum.js
@@ -0,0 +1,18 @@
+"use strict";
+// --- 1. Two Sum --- //
+/* Given an array of integers nums and an integer target, return indices of the two numbers such that they add up to target.
+You may assume that each input would have exactly one solution, and you may not use the same element twice.
+You can return the answer in any order.*/
+const twoSum = (nums, target) => {
+    const answer = [];
+    for (let i = 0; i < nums.length; i++) {
+        let num1 = nums[i];
+        for (let j = 0; j < nums.length; j++) {
+            let num2 = nums[j];
+            if (num1 + num2 === target && i !== j) {
+                answer.push(i, j);
+            }
+        }
+    }
+    return answer;
+};

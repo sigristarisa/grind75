4c3b2604d7ddb5c8205671f3cdd5fd4969c997ec
sigristarisa <arisasigrist.ch@gmail.com>
2022-11-08 15:52:36 +0100

    added description of task

--- 2_valid-parentheses/2_valid-parentheses.ts
+++ 2_valid-parentheses/2_valid-parentheses.ts
@@ -1,4 +1,11 @@
-function isValid(s: string): boolean {
+// --- 2. Valid Parentheses --- //
+/* Given a string s containing just the characters '(', ')', '{', '}', '[' and ']', determine if the input string is valid.
+An input string is valid if:
+Open brackets must be closed by the same type of brackets.
+Open brackets must be closed in the correct order.
+Every close bracket has a corresponding open bracket of the same type. */
+
+const isValid = (s: string): boolean => {
   const sArr: string[] = [];
   const bracketObj: Record<string, string> = {
     "(": ")",
@@ -7,15 +14,11 @@ function isValid(s: string): boolean {
   };
 
   for (const char of s) {
-    // if char is "(", "{", or "[", push ")", "}", "]" to the array
     if (bracketObj[char]) {
       sArr.push(bracketObj[char]);
-
-      // if char is ")", "}", or "]", and the last element in the array is not, return false
     } else if (sArr.pop() !== char) {
       return false;
     }
   }
-  // if all of the bracket matches, then the array length should be 0
   return sArr.length === 0;
-}
+};

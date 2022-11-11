5af53b4fda863f518c7dce971fd08cceb60d3ca9
sigristarisa <arisasigrist.ch@gmail.com>
2022-11-11 16:07:53 +0100

    added description of day 5

--- 5_valid-palindrome/5_valid-palindrome.ts
+++ 5_valid-palindrome/5_valid-palindrome.ts
@@ -1,3 +1,8 @@
+// --- 5. Valid Palindrome --- //
+/* A phrase is a palindrome if, after converting all uppercase letters into lowercase letters and removing all non-alphanumeric characters, it reads the same forward and backward. 
+Alphanumeric characters include letters and numbers.
+Given a string s, return true if it is a palindrome, or false otherwise.*/
+
 const isPalindrome = (s: string): boolean => {
   const nonAlphanumeric: RegExp = /[^A-Za-z0-9]/g;
   s = s.replace(nonAlphanumeric, "").toLowerCase();
